f283d0e6415f54d73fdb61b7d9115d1988281289
SanzharShadybekov <shadubekovs777@gmail.com>
2023-02-03 21:03:09 +0600

    added user list

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import permissions
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -56,3 +56,8 @@ class LogoutView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response('Successfully logged out!', status=200)
+
+
+class UserListApiView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
